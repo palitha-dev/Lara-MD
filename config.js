2172db3c52407aa7ed9097e1383ecceb6cbe22bc
palitha-dev <najithpalithamunasingha@gmail.com>
2025-01-03 17:58:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "S2BEAJaI#R2yrgtVdXvfu8Fk1aMhL3deIo5cEhyLaZazHN6MUvEY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
